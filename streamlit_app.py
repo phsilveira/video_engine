4aa6ed3675982eb4f5e3feea6c48f6d5432b793c
Paulo <phsilveira.henrique@gmail.com>
2024-06-18 01:11:45 -0300

    feat/show_error_message

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,17 @@ import pandas as pd
 import json
 import os
 import time
+from collections import deque
 
 
-def generate_scene_json(row, composition, template="Cursos_Template_B", project_template_path='C:\\Users\Administrator\\Downloads\\Cursos_Template_B_v3\\', output_path="C:\\Users\\Administrator\\Documents\\video_engine\\output\\"):
+def generate_scene_json(
+        row, 
+        composition, 
+        template="Cursos_Template_B", 
+        project_template_path='C:\\Users\Administrator\\Downloads\\Cursos_Template_B_v3\\', 
+        output_path=".\\output\\"
+    ):
+
     output_path = f"{output_path}{template}__{composition}.mp4"
     base_contract = {
         "template": {
@@ -147,21 +155,26 @@ def generate_scene_json(row, composition, template="Cursos_Template_B", project_
     return base_contract, output_path
 
 def main():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
     st.title('Video Engine - Batch Render')
+
     templates = {
         'Template C': {
             'name':'Cursos_Template_C',
-            'project_template_path':'C:\\Users\Administrator\\Downloads\\Cursos_Template_C_v7\\',
-            'audio_path':'C:\\Users\\Administrator\\Documents\\video_engine\\example\\template_c\\input_audio.mp4'
+            'project_template_path':'D:\\Downloads\\Cursos_Template_C_v7\\',
+            'audio_path':'.\\example\\template_c\\input_audio.mp4'
         },
         'Template B': {
             'name':'Cursos_Template_B',
-            'project_template_path':'C:\\Users\Administrator\\Downloads\\Cursos_Template_B_v3\\',
-            'audio_path':'C:\\Users\\Administrator\\Documents\\video_engine\\example\\template_b\\input_audio.mp3'
+            'project_template_path':'D:\\Downloads\\Cursos_Template_B_v3\\',
+            'audio_path':'.\\example\\template_b\\input_audio.mp3'
         },
-        # '1.4.4 - Course summary - Quiz interativo': {
-        #     ''
-        # }
+        '1.4.4 - Course summary - Quiz interativo': {
+            'name': '144-course-summary-quiz-interativopre-rendermotor',
+            'project_template_path': 'D:\\Downloads\\144-course-summary-quiz-interativopre-rendermotor\\',
+            'audio_path': '.\\example\\144-course-summary-quiz-interativopre-rendermotor\\144-course-summary-quiz-interativopre-rendermotor.mp3',
+        }
     }
 
     selected_template = st.selectbox('Select a template', list(templates.keys()))
@@ -180,9 +193,8 @@ def main():
 
         for _, row in df.iterrows():
             target = row['target']
-            scene_dict, output_video_path = generate_scene_json(row, row['target'], templates[selected_template]['name'], templates[selected_template]['project_template_path'],)
+            scene_dict, output_video_path = generate_scene_json(row, row['target'], templates[selected_template]['name'], templates[selected_template]['project_template_path'], current_dir + '\\output\\')
 
-            # output_file_path = f'output/{templates[selected_template]["name"]}/{target}.json'
             output_file_path = f'output/{target}.json'
 
             # create a folder if it does not exist
@@ -208,84 +220,99 @@ def main():
 
     if uploaded_file is not None and st.button('Start Process'):
         process_running = True
+        error_occurred = False
         start = time.time()
 
         for scene in scenes_script_to_render:
+            if error_occurred:
+                break
+
             with st.spinner(f'Running the batch file for {scene}...'):
-                command = f'C:\\Users\\Administrator\\Documents\\video_engine\\nexrender-cli-win64.exe --file {scene} --binary "C:\\Program Files\\Adobe\\Adobe After Effects 2024\\Support Files\\aerender.exe" --skip-cleanup'
+                command = f'.\\nexrender-cli-win64.exe --file {scene} --binary "C:\\Program Files\\Adobe\\Adobe After Effects 2024\\Support Files\\aerender.exe"'
+                process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+                output_area = st.empty()
+
+                last_five_lines = deque(maxlen=5)
+
+                for line in iter(process.stdout.readline, b''):
+                    decoded_line = line.decode()
+                    last_five_lines.append(decoded_line)
+
+                    output_area.text('\n'.join(last_five_lines))
+                    print(decoded_line)
+
+                    if 'error' in decoded_line:
+                        process.terminate()
+                        error_occurred = True
+                        st.error(f"An error occurred during the process: {decoded_line}")
+                        break
+
+                process.stdout.close()
+
+        if not error_occurred:
+
+            with st.spinner('Merging the videos...'):
+                command = f'ffmpeg -y -f concat -safe 0 -i ./output/scenes_to_render.txt -c copy ./output/output.mp4'
+                process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                output_area = st.empty()
+
+                for line in iter(process.stdout.readline, b''):
+                    output_area.text(line.decode())
+                    print(line.decode())
+                    if 'rendering complete!' in line.decode():
+                        break
+                process.stdout.close()
+
+                command = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ./output/output.mp4'
+                process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+                video_duration = process.stdout.read().decode()
+                video_duration = float(video_duration)
+                fade_out_start = video_duration - 2
+
+                command = f'ffmpeg -y -stream_loop -1 -i {templates[selected_template]["audio_path"]} -i ./output/output.mp4 -filter_complex "[0:a]volume=1[a];[a]afade=t=out:st={fade_out_start}:d=2[a1];[1:a][a1]amix=inputs=2:duration=first:dropout_transition=2[aout]" -map 1:v -map "[aout]" -c:v copy -c:a aac ./output/output2.mp4'
                 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
                 output_area = st.empty()
 
                 for line in iter(process.stdout.readline, b''):
                     output_area.text(line.decode())
-                    # output_area.text(f"Comp {scene}: {line.decode()}")
                     print(line.decode())
                     if 'rendering complete!' in line.decode():
                         break
                 process.stdout.close()
 
-        with st.spinner('Merging the videos...'):
-            command = f'ffmpeg -y -f concat -safe 0 -i C:/Users/Administrator/Documents/video_engine/output/scenes_to_render.txt -c copy C:/Users/Administrator/Documents/video_engine/output/output.mp4'
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            output_area = st.empty()
+                command = 'ffmpeg -y -f concat -safe 0 -i ./output/final_merge.txt -c copy ./output/output_final.mp4'
+                process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-            for line in iter(process.stdout.readline, b''):
-                output_area.text(line.decode())
-                print(line.decode())
-                if 'rendering complete!' in line.decode():
-                    break
-            process.stdout.close()
+                output_area = st.empty()
 
-            command = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 C:/Users/Administrator/Documents/video_engine/output/output.mp4'
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                for line in iter(process.stdout.readline, b''):
+                    output_area.text(line.decode())
+                    print(line.decode())
+                    if 'rendering complete!' in line.decode():
+                        break
+                process.stdout.close()
 
-            video_duration = process.stdout.read().decode()
-            video_duration = float(video_duration)
-            fade_out_start = video_duration - 2
+            process_running = False
+            end = time.time()
 
-            command = f'ffmpeg -y -stream_loop -1 -i {templates[selected_template]["audio_path"]} -i C:/Users/Administrator/Documents/video_engine/output/output.mp4 -filter_complex "[0:a]volume=1[a];[a]afade=t=out:st={fade_out_start}:d=2[a1];[1:a][a1]amix=inputs=2:duration=first:dropout_transition=2[aout]" -map 1:v -map "[aout]" -c:v copy -c:a aac C:/Users/Administrator/Documents/video_engine/output/output2.mp4'
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            st.success(f'Render complete successfully in {round(end-start, 2)}!')
 
-            output_area = st.empty()
+            # Display the video
+            video_file = './output/output_final.mp4'
+            st.video(video_file)
 
-            for line in iter(process.stdout.readline, b''):
-                output_area.text(line.decode())
-                print(line.decode())
-                if 'rendering complete!' in line.decode():
-                    break
-            process.stdout.close()
-
-            command = 'ffmpeg -y -f concat -safe 0 -i C:/Users/Administrator/Documents/video_engine/output/final_merge.txt -c copy C:/Users/Administrator/Documents/video_engine/output/output_final.mp4'
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-            output_area = st.empty()
-
-            for line in iter(process.stdout.readline, b''):
-                output_area.text(line.decode())
-                print(line.decode())
-                if 'rendering complete!' in line.decode():
-                    break
-            process.stdout.close()
-
-        process_running = False
-        end = time.time()
-
-        st.success(f'Render complete successfully in {round(end-start, 2)}!')
-
-        # Display the video
-        video_file = 'C:/Users/Administrator/Documents/video_engine/output/output_final.mp4'
-        st.video(video_file)
-
-        # Allow the user to download the video
-        with open(video_file, 'rb') as f:
-            video_bytes = f.read()
-        st.download_button(
-            label="Download video",
-            data=video_bytes,
-            file_name='output_final.mp4',
-            mime='video/mp4'
-        )
+            # Allow the user to download the video
+            with open(video_file, 'rb') as f:
+                video_bytes = f.read()
+            st.download_button(
+                label="Download video",
+                data=video_bytes,
+                file_name='output_final.mp4',
+                mime='video/mp4'
+            )
 
 if __name__ == "__main__":
     main()
